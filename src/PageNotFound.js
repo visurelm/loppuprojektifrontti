7409b47c3388cf98459e8a4c3b77e164aafdde9f
visurelm <elmo.visuri@gmail.com>
2018-08-28 13:11:28 +0300

    testi

---
+++ src/PageNotFound.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PageNotFound extends Component{
+    render() {
+        return (
+            <div>
+                <p>Page not found... / Sivvuuu eeee löyyyyyyyyyyyöyöyöy</p>
+            </div>
+        );
+    }
+}
+
+export default PageNotFound;
