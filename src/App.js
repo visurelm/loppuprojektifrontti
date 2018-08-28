4903280b788cdab9df43e87fe4dd6e2301f96290
visurelm <elmo.visuri@gmail.com>
2018-08-28 08:58:05 +0300

    added more pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route} from  'react-router-dom';
+import { Grid } from 'react-bootstrap';
 import './App.css';
 import Home from './components/Home';
+import Users from './components/Users';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-          <Home/>
-      </div>
-    );
-  }
-}
+
+const App = appProps => (
+    <Router>
+        <div className="App">
+            <Grid>
+                <switch>
+                    <Route exact name="index" path="/" component={Home}/>
+                    <Route path="/Users" component={Users}/>
+                </switch>
+            </Grid>
+        </div>
+    </Router>
+)
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//           <Home/>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
