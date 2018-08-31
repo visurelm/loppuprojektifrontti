55a26758db0980e664709316778c44cb77c94693
visurelm <elmo.visuri@gmail.com>
2018-08-31 09:14:16 +0300

    lisätty interaktiivisuus yläpalkkiin

--- src/Navigation.js
+++ src/Navigation.js
@@ -5,7 +5,7 @@ import {Navbar, Nav, NavItem, NavDropdown, MenuItem} from 'react-bootstrap';
 class Navigation extends Component {
     render() {
         return (
-            <Navbar staticTop>
+            <Navbar staticTop inverse collapseOnSelect>
                 <Navbar.Header>
                     <Navbar.Brand>
                         <a href="/">Etusivu</a>
@@ -17,10 +17,12 @@ class Navigation extends Component {
                         <NavItem eventKey={1} href="/">Etusivu</NavItem>
                         <NavItem eventKey={2} href="/Users">Users</NavItem>
                         <NavItem eventKey={3} href="/MyOwnPage">Samoin tähän</NavItem>
-                        <NavItem eventKey={3} href="/Jermuiluja">Jermujen pesä</NavItem>
-                        <NavDropdown eventKey={5} title="Tähn tulee pelit" id="pelidropdown"></NavDropdown>
+                        <NavItem eventKey={4} href="/Jermuiluja">Jermujen pesä</NavItem>
+                        <NavDropdown eventKey={5} title="Tähn tulee pelit" id="pelidropdown">
+                            <MenuItem eventKey={5.1}>Muistipeli</MenuItem>
+                        </NavDropdown>
                         {!this.props.auth.isAuthenticated() &&
-                            <button onClick={this.props.auth.login}>Login</button>}
+                        <button onClick={this.props.auth.login}>Login</button>}
                         {this.props.auth.isAuthenticated() && <button onClick={this.props.auth.logout}>Logout</button>}
 
                     </Nav>
