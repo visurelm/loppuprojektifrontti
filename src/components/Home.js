c33ad78b14749e377dd70b9b2d86f90d5166db50
jounikoo <jouni.kullervo.kari@gmail.com>
2018-08-31 12:08:46 +0300

    navcube responsiivinen eka versio

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,7 +10,7 @@ class Home extends Component{
             <div className="Homepage">
                 <Navcubes/>
                 {!this.props.auth.isAuthenticated() && <h1>Moro, kirjaudu!</h1>}
-                {this.props.auth.isAuthenticated() && <h1>kaikkee shittii.. alkuun juuserit</h1>}
+                {this.props.auth.isAuthenticated() && <h1>kaikkee... alkuun juuserit</h1>}
                 {this.props.auth.isAuthenticated() && < Users />}
 
             </div>
--- src/components/Navcubes.js
+++ src/components/Navcubes.js
@@ -1,22 +1,20 @@
 import React from 'react'
+import {Row, Col} from 'react-bootstrap';
 import letterE from '../images/letterE.png';
 import letterL from '../images/letterL.png';
 import letterS from '../images/letterS.png';
 import letterA from '../images/letterA.png';
 import './Navcubes.css';
 
-const imgScaling = {
-    width: 'auto',
-    height: 'auto',
-};
 
 const Navcubes = () => (
-    <div style = {imgScaling}>
-        <a href="/"><img src={letterE} alt="letterE" /></a>
-        <a href="/Logout"><img src={letterL} alt="letterL" /></a>
-        <a href="/users"><img src={letterS} alt="letterS" /></a>
-        <a href="/MyOwnPage"><img src={letterA} alt="letterA" /></a>
-        {/*<img src={letterE} alt="letterE"/>*/}
+    <div className="navcubes">
+        <Row>
+            <Col sm={3}><a href="/"><img src={letterE} alt="letterE"/></a></Col>
+            <Col sm={3}><a href="/Logout"><img src={letterL} alt="letterL"/></a></Col>
+            <Col sm={3}><a href="/users"><img src={letterS} alt="letterS"/></a></Col>
+            <Col sm={3}><a href="/MyOwnPage"><img src={letterA} alt="letterA"/></a></Col>
+        </Row> {/*<img src={letterE} alt="letterE"/>*/}
         {/*<img src={letterL} alt="letterL"/>*/}
         {/*<img src={letterS} alt="letterS"/>*/}
         {/*<img src={letterA} alt="letterA"/>*/}
